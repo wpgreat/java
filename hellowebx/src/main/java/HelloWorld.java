d6240a0488693ec1968e2956f68148acca1fbda2
wupeng <wupeng05@chinasoftinc.com>
2016-04-05 10:30:27 +0800

    新增Java文件

---
+++ hellowebx/src/main/java/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author icss
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
