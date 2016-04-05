3b10305c14d16a617b4e600ed95d354c9d12b710
wupeng <wupeng05@chinasoftinc.com>
2016-04-05 11:29:40 +0800

    新增文件

---
+++ hellowebx/src/main/java/HelloWebx.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author icss
+ *
+ */
+public class HelloWebx {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Webx!");
+	}
+
+}
