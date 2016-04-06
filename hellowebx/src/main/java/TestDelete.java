4ca52c13b9f5cdfabb22f6155611553bd2a7a540
wupeng <wupeng05@chinasoftinc.com>
2016-04-06 10:18:30 +0800

    测试删除文件

---
+++ hellowebx/src/main/java/TestDelete.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author icss
+ *
+ */
+public class TestDelete {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Test Delete!");
+	}
+
+}
